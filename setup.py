afa7f4c529d798549fbd9459fe6134231f26e5bc
Mike <xxmixanikxx@gmail.com>
2022-04-10 23:10:45 +0300

    initial

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='ibsplib',
+    version='0.1.0',
+    author='Mix-Anik',
+    description='Python library for working with Quake 3 IBSP structures',
+    packages=['ibsplib'],
+    license='MIT',
+    url='https://github.com/Mix-Anik/ibsplib',
+    download_url='',
+    keywords=['quake 3', 'q3', 'IBSP', 'BSP']
+)
